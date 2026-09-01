fb4d182b8eb86adb0bb08c288a82c574b90012a5
MiyokoShinzu <71174588+MiyokoShinzu@users.noreply.github.com>
2026-09-01 22:49:11 +0800

    Update logout.php

--- student/logout.php
+++ student/logout.php
@@ -24,7 +24,7 @@ if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
 
     setcookie(
-        session_name(),
+        session_name(), 
         "",
         time() - 42000,
         $params["path"],
