0b1282c657947e1df0d8d04ad040e831ce59dc5d
MiyokoShinzu <71174588+MiyokoShinzu@users.noreply.github.com>
2026-09-11 19:52:53 +0800

    Update paymongo.php

--- src/paymongo.php
+++ src/paymongo.php
@@ -20,10 +20,12 @@
 ========================================================== */
 
 $paymongoSecretKey =
-    "sk_live_i4inHTBRwSBTEsUHiUvqVjk8" ;
+    // "sk_live_i4inHTBRwSBTEsUHiUvqVjk8" ;
+    "sk_test_ge3huoytcE7BgpT2EPaDaj1g";
 
 $paymongoPublicKey =
-    "pk_live_AidrFnT47Jdk6FJBVZC11WAY";
+    // "pk_live_AidrFnT47Jdk6FJBVZC11WAY";
+    "pk_test_cd6WVe7HBqyuoYYApNMZJvJ2";
 
 
 /* =========================================================
